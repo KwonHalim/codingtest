37aa46459f31deab886aed0320e4d52d07120379
KwonHalim <hr_blizzard@naver.com>
2026-03-10 10:33:08 +0900

    Refactor shortest path algorithm in 최단경로.py
    Updated distance initialization and added debug prints.

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -13,29 +13,37 @@ for _ in range(E):
     graph[u].append((v, w))
 
 INF = int(1e9)
-dist = [INF] * (V + 1)
+# 각 위치에서 도달할 수 있는 노드, 정점까지의 거리 쌍
+# [[], [(2, 2), (3, 3)], [(3, 4), (4, 5)], [(4, 6)], [], [(1, 1)]]
+# 위치에서 가는 거리, 정점
+#heapq -> (1,2)
+
+dist = [float('INF')] * (V+1)
 dist[K] = 0
 
-# print(graph)
+# 다음에 갈 위치 저장
+heap= []
 
-heap = []
-# heap: 해당노드까지의 거리, 도착노드
 heapq.heappush(heap,(0,K))
-# Grpah는 노드, 거리 순
 
 while heap:
+    print(heap)
+    print(dist)
+    # distance는 지금까지 node까지의 거리
     distance, node = heapq.heappop(heap)
     if distance > dist[node]:
         continue
 
-    for new_node, weight in graph[node]:
+    for next_node, weight in graph[node]:
+        # 현재(현재(node)의 위치까지의 거리) + next_node까지의 거리 계산
         new_dist = weight + distance
+        # 위의 거리가 이미 저장되어있는 거리보다 짧다면 업데이트
+        if dist[next_node] > new_dist:
+            dist[next_node] = new_dist
+            heapq.heappush(heap,(new_dist,next_node))
 
-        if new_dist < dist[new_node]:
-            dist[new_node] = new_dist
-            heapq.heappush(heap,(new_dist, new_node))
 
-for i in range(1, V + 1):
+for i in range(1, V+1):
     if dist[i] == INF:
         print("INF")
     else:
