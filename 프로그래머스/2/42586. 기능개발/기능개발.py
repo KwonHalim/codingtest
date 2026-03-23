04ae1987ac56373f35d5dd2a8ba45aa5a0833a1c
KwonHalim <halim7753@gmail.com>
2026-03-23 11:52:46 +0900

    [level 2] Title: 기능개발, Time: 0.82 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -2,26 +2,16 @@ def solution(progresses, speeds):
     answer = []
     idx = 0
     day = 0
-    while idx < len(progresses)-1:
+    while idx < len(progresses):
         day = 0
         while progresses[idx] < 100:
             for i in range(idx,len(progresses)):
                 progresses[i] += speeds[i]
-        while progresses[idx] >= 100:
-            if idx >= len(progresses)-1:
-                if progresses[idx] >= 100:
-                    day+=1
-                    break
-            idx+=1
+                
+                
+        while idx < len(progresses) and progresses[idx] >= 100:
+            idx +=1
             day+=1
         answer.append(day)
-        print(progresses, answer, idx)
-    day = 1
-    
-    if progresses[-1] < 100:
-        while progresses[-1] >= 100:
-            day+=1
-            progresses[-1] += speeds[-1]
-        answer.append(day)
-    
+        
     return answer
